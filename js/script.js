ac4c0d72e9008cb14352b62c4c74baec108767f3
Gabrielhidro <gabrielhidro.dev@gmail.com>
2020-11-23 17:00:49 -0300

    imc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
-console.log('fui')
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Gabriel nutrição';
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for(var i = 0; i < pacientes.length; i++){
+
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector('.info-peso');
+  var peso = tdPeso.textContent
+  
+  var tdAltura = paciente.querySelector('.info-altura')
+  var altura = tdAltura.textContent
+  
+  var tdImc = paciente.querySelector('.info-imc');
+  
+  var pesoValido = true;
+  var alturaValida = true;
+  
+  if(peso <= 0 || peso >= 1000){
+    pesoValido = false;
+    tdImc.textContent = 'peso inválido'
+    paciente.classList.add("paciente-invalido")
+  }
+  
+  if(altura <= 0 || altura >= 4.00){
+    alturaValida = false;
+    tdImc.textContent = 'altura inválida'
+    paciente.classList.add("paciente-invalido")
+  }
+  
+  if(pesoValido && alturaValida){
+    var imc = calculaImc(peso,altura)
+    tdImc.textContent = imc;
+  }
+}
+
+function calculaImc(peso,altura){
+  var imc = 0;
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
+
+
+
+
+
